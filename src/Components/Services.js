b25b17b39c6c2e4add47ddd513cea23a5b841af6
dannywarekuromo <dannywarekuromo@gmail.com>
2022-09-12 19:21:26 +0100

    Component style update

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -6,44 +6,34 @@ import Furniture from "../Images/Furniture.png";
 import Products from "../Images/Products.png";
 import Search from "../Images/png/003-search.png";
 
-export default function Services() {
-  function Layer() {
-    return (
-      <Link className="layer" to="/Projects">
-        <div className="search-div">
-          <img src={Search} alt="Search" className="service-search" />
-        </div>
-      </Link>
-    );
-  }
+function Layer() {
+  return (
+    <Link className="layer" to="/Projects">
+      <div className="search-div">
+        <img src={Search} alt="Search" className="service-search" />
+      </div>
+    </Link>
+  );
+}
 
-  function Service({ children, image }) {
+export default function Services() {
+
+  function Service(props) {
     return (
       <div className="service">
         <Layer />
-        <h4 className="service-head">{children}</h4>
+        <h4 className="service-head">{props.head}</h4>
+        <img src={props.source} alt={props.alternate} className="service-img" />
       </div>
     );
   }
 
   return (
     <section className="services">
-      <Service>
-        Architecture
-        <img src={Architecture} alt="Architecture" className="service-img" />
-      </Service>
-      <Service>
-        Interior
-        <img src={Interior} alt="Interior" className="service-img" />
-      </Service>
-      <Service>
-        Furniture
-        <img src={Furniture} alt="Furniture" className="service-img" />
-      </Service>
-      <Service>
-        Products
-        <img src={Products} alt="Products" className="service-img" />
-      </Service>
+      <Service head="Architecture" source={Architecture} alternate="Architecture"/>
+      <Service head="Interior" source={Interior} alternate="Interior"/>
+      <Service head="Furniture" source={Furniture} alternate="Furniture"/>
+      <Service head="Products" source={Products} alternate="Products"/>
     </section>
   );
 }
